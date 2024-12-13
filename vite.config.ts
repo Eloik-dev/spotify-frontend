be87cfee24d90b8b4bc1838be4142d4abac3bd9a
Eloik-dev <eloik.rousseau@gmail.com>
2024-12-13 18:33:48 -0500

    Render

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
     },
-  }
-})
+    server: {
+      host: '0.0.0.0',
+      port: Number(env.PORT) || 80,
+    },
+  };
+});
